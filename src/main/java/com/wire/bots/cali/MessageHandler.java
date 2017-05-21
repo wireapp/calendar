4dcfc54e9551bc1643841337eaa3ce366794f1c4
dkovacevic <dejankov@gmail.com>
2017-05-21 22:35:50 +0200

    Code refactoring

--- src/main/java/com/wire/bots/cali/MessageHandler.java
+++ src/main/java/com/wire/bots/cali/MessageHandler.java
@@ -32,75 +32,14 @@ import com.wire.bots.sdk.assets.Picture;
 import com.wire.bots.sdk.models.AssetKey;
 import com.wire.bots.sdk.models.TextMessage;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.IOException;
-import java.util.*;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public class MessageHandler extends MessageHandlerBase {
-    private static final int REMIND_IN = 15;
-    private static final int PERIOD = 15;
-
     private final ScheduledExecutorService executor = new ScheduledThreadPoolExecutor(4);
-    private final Timer timer = new Timer();
-    private final HashMap<String, Event> remindersMap = new HashMap<>();
-
-    private final ClientRepo repo;
 
     MessageHandler(ClientRepo repo) {
-        this.repo = repo;
-
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                for (WireClient client : getClients()) {
-                    fetchEvents(client, 3);
-                }
-            }
-        }, TimeUnit.MINUTES.toMillis(1), TimeUnit.MINUTES.toMillis(PERIOD));
-    }
-
-    private void fetchEvents(final WireClient wireClient, int count) {
-        try {
-            Calendar service = CalendarAPI.getCalendarService(wireClient.getId());
-            DateTime now = new DateTime(System.currentTimeMillis());
-            Events events = service.events().list("primary")
-                    .setMaxResults(count)
-                    .setTimeMin(now)
-                    .setOrderBy("startTime")
-                    .setSingleEvents(true)
-                    .execute();
-
-            for (final Event event : events.getItems()) {
-                String id = String.format("%s-%s", wireClient.getId(), event.getId());
-                if (remindersMap.put(id, event) == null) {
-                    scheduleReminder(wireClient, event);
-                }
-            }
-
-        } catch (IOException e) {
-            Logger.warning(e.getLocalizedMessage());
-        }
-    }
-
-    private void scheduleReminder(final WireClient wireClient, final Event event) {
-        DateTime start = event.getStart().getDateTime();
-        long startUTC = start.getValue() - start.getTimeZoneShift();
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                try {
-                    String msg = String.format("**%s** in **%d** minutes", event.getSummary(), REMIND_IN);
-                    wireClient.sendText(msg);
-                } catch (Exception e) {
-                    Logger.warning(e.getLocalizedMessage());
-                }
-            }
-        }, new Date(startUTC - TimeUnit.MINUTES.toMillis(REMIND_IN)));
+        new AlertManager(repo);
     }
 
     @Override
@@ -133,37 +72,35 @@ public class MessageHandler extends MessageHandlerBase {
                 String authUrl = CalendarAPI.getAuthUrl(client.getId());
                 client.sendText(authUrl);
             } else if (msg.getText().equalsIgnoreCase("/list")) {
-                try {
-                    Calendar service = CalendarAPI.getCalendarService(client.getId());
-
-                    String text = listEvents(service);
-                    client.sendText(text);
-                } catch (GoogleJsonResponseException ex) {
-                    Logger.warning(ex.getLocalizedMessage());
-                    client.sendText("Failed to connect to Google Calendar. Have you signed in? Type: `/auth` and sign in " +
-                            "to your Google account");
-                }
+                showCalendar(client);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private String listEvents(Calendar service) throws java.io.IOException {
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("primary")
-                .setMaxResults(5)
-                .setTimeMin(now)
-                .setOrderBy("startTime")
-                .setSingleEvents(true)
-                .execute();
-        StringBuilder sb = new StringBuilder("Upcoming events:\n");
-        for (Event event : events.getItems()) {
-            EventDateTime eventStart = event.getStart();
-            DateTime start = eventStart.getDateTime();
-            sb.append(String.format("**%s** at %s\n", event.getSummary(), start));
+    private void showCalendar(WireClient client) throws Exception {
+        try {
+            Calendar service = CalendarAPI.getCalendarService(client.getId());
+            DateTime now = new DateTime(System.currentTimeMillis());
+            Events events = service.events().list("primary")
+                    .setMaxResults(5)
+                    .setTimeMin(now)
+                    .setOrderBy("startTime")
+                    .setSingleEvents(true)
+                    .execute();
+            StringBuilder sb = new StringBuilder("Upcoming events:\n");
+            for (Event event : events.getItems()) {
+                EventDateTime eventStart = event.getStart();
+                DateTime start = eventStart.getDateTime();
+                sb.append(String.format("**%s** at %s\n", event.getSummary(), start));
+            }
+            client.sendText(sb.toString());
+        } catch (GoogleJsonResponseException ex) {
+            Logger.warning(ex.getLocalizedMessage());
+            client.sendText("Failed to connect to Google Calendar. Have you signed in? Type: `/auth` and sign in " +
+                    "to your Google account");
         }
-        return sb.toString();
     }
 
     private Picture uploadPreview(WireClient client, String imgUrl) throws Exception {
@@ -173,21 +110,5 @@ public class MessageHandler extends MessageHandlerBase {
         AssetKey assetKey = client.uploadAsset(preview);
         preview.setAssetKey(assetKey.key);
         return preview;
-    }
-
-    private ArrayList<WireClient> getClients() {
-        final ArrayList<WireClient> ret = new ArrayList<>();
-        File dir = new File(repo.getPath());
-        File[] files = dir.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File file) {
-                String botId = file.getName();
-                WireClient wireClient = repo.getWireClient(botId);
-                if (wireClient != null)
-                    ret.add(wireClient);
-                return wireClient != null;
-            }
-        });
-        return ret;
     }
 }
