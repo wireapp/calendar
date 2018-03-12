2253bf64a6b1832d2be7802050c2a7daa12c4d2a
Dusan Stevanovic <dule@wire.com>
2018-03-12 22:54:03 +0100

    Use proper config API for fetching calls config

--- src/main/java/com/wire/blender/Blender.java
+++ src/main/java/com/wire/blender/Blender.java
@@ -1,41 +1,51 @@
 package com.wire.blender;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Blender {
-    static {
-        System.loadLibrary("blender"); // Load native library at runtime
-    }
+	static {
+		System.loadLibrary("blender"); // Load native library at runtime
+	}
 
-    private long blenderPointer;
+	private long blenderPointer;
 
-    private List<BlenderListener> listeners = new ArrayList<>();
+	private List<BlenderListener> listeners = new ArrayList<>();
+	
+	public void log(String msg) {
+		System.out.println("JAVA:Blender: " + msg);
+	}
 
-    public void log(String msg) {
-        System.out.println("JAVA:Blender: " + msg);
-    }
+	public void registerListener(BlenderListener listener) {
+		listeners.add(listener);
+	}
 
-    public void registerListener(BlenderListener listener) {
-        listeners.add(listener);
-    }
 
-    private void onCallingMessage(String id,
-                                  String userId,
-                                  String clientId,
-                                  String peerId,
-                                  String peerClientId,
-                                  String content,
-                                  boolean trans)
-    {
-        for (BlenderListener lsnr : listeners) {
-            lsnr.onCallingMessage(id, userId, clientId,
-                    peerId, peerClientId,
-                    content, trans);
-        }
-    }
+	private void onConfigRequest()
+	{
+		for (BlenderListener lsnr : listeners) {
+			lsnr.onConfigRequest();
+		}
+	}
 
-    public native void recvMessage(String convid, String userid,
-                                   String clientid, String content);
-    public native void init(String config, String userid, String clientid);
+	
+	private void onCallingMessage(String id,
+				      String userId,
+				      String clientId,
+				      String peerId,
+				      String peerClientId,
+				      String content,
+				      boolean trans)
+	{
+		for (BlenderListener lsnr : listeners) {
+			lsnr.onCallingMessage(id, userId, clientId,
+					      peerId, peerClientId,
+					      content, trans);
+		}
+	}
+ 
+	public native void recvConfig(String config);
+	public native void recvMessage(String convid, String userid,
+				       String clientid, String content);
+	public native void init(String config, String userid, String clientid);
 }
--- src/main/java/com/wire/blender/BlenderListener.java
+++ src/main/java/com/wire/blender/BlenderListener.java
@@ -1,12 +1,14 @@
 package com.wire.blender;
 
 public interface BlenderListener {
+	public void onCallingMessage(String id,
+				     String userId,
+				     String clientId,
+				     String peerId,
+				     String peerClientId,
+				     String content,
+				     boolean trans);
 
-    void onCallingMessage(String id,
-                          String userId,
-                          String clientId,
-                          String peerId,
-                          String peerClientId,
-                          String content,
-                          boolean trans);
+	public void onConfigRequest();
 }
+	
